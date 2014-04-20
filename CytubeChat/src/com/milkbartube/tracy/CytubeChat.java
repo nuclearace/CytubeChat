3505102f0850c729fd430f2e749fc62a4f55e4c6
Erik <nuclear.ace@gmail.com>
2014-04-19 23:26:18 -0400

    Small tweak

--- CytubeChat/src/com/milkbartube/tracy/CytubeChat.java
+++ CytubeChat/src/com/milkbartube/tracy/CytubeChat.java
@@ -8,11 +8,9 @@ public class CytubeChat {
     public static void main(String args[]) {
 
 	String os = System.getProperty("os.name");
-	System.out.println(os);
+	System.out.println("Starting CytubeChat");
 	if (os.equals("Mac OS X")) {
 	    System.setProperty("apple.laf.useScreenMenuBar", "true");
-	    System.setProperty("com.apple.mrj.application.apple.menu.about.name"
-		    , "Cytube Chat");
 	    try {
 		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 	    } catch (ClassNotFoundException | InstantiationException
